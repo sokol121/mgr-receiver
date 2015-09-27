d75ff991ec83d20ceb7f0d649ce46e1c87c1c4f2
sokol121 <sokol121@gmail.com>
2015-09-27 22:10:18 +0200

    poprawki

--- DataReceiver/src/pl/edu/pw/DataReceiver.java
+++ DataReceiver/src/pl/edu/pw/DataReceiver.java
@@ -1,47 +1,60 @@
 package pl.edu.pw;
 
 import static java.lang.System.out;
-import net.tinyos.message.*;
-import net.tinyos.util.*;
+
+import java.io.IOException;
+
+import net.tinyos.message.Message;
+import net.tinyos.message.MessageListener;
+import net.tinyos.message.MoteIF;
+import net.tinyos.packet.BuildSource;
+import net.tinyos.packet.PhoenixSource;
+import net.tinyos.util.PrintStreamMessenger;
 import pl.edu.pw.Sensors.Mts400;
 import pl.edu.pw.dbWriters.MoteMsgDbWriter;
-import net.tinyos.packet.*;
 
 class DataReceiver implements MessageListener{
 	private PhoenixSource phoenix;
 	private MoteIF mif;
 	private static DataReceiver hy;
 	private Mts400 mts400 = new Mts400();
+	MoteMsgDbWriter writer = new MoteMsgDbWriter();
+	Integer i = 0;
 		
 	public DataReceiver(final String source){
 		phoenix=BuildSource.makePhoenix(source, PrintStreamMessenger.err);
 		mif = new MoteIF(phoenix);
 		mif.registerListener(new MoteMsg(),this);
+		System.out.println("done");
 	}
 	
 	public void messageReceived(int dest_addr,Message msg){
 		if(msg instanceof MoteMsg){
 			MoteMsg results = (MoteMsg)msg;
-			MoteMsgDbWriter writer = new MoteMsgDbWriter();
 			writer.writeReadingToDb(results);
-			int[] taosCalcData = null;
-			double[] sensirionCalcData=null;
-			results.get_nodeId();
-			out.println("The measured results are ");
-			out.println();
-			out.println("Accelerometer X axis:   "+results.get_AccelX_data());
-			out.println("Accelerometer Y axis:   "+results.get_AccelY_data());
-			out.println("Intersema temperature:  "+(double)results.getElement_Intersema_data(0)/10);
-			out.println("Intersema pressure:     "+(double)results.getElement_Intersema_data(1)/10);
-			sensirionCalcData= mts400.calculateSensirion(results.get_tempVal(),results.get_humVal());
-			out.printf("Sensirion temperature:  %.2f\n",sensirionCalcData[0]);
-			out.printf("Sensirion humidity:     %.2f\n",sensirionCalcData[1]);
-			taosCalcData=mts400.calculateTaos(results.get_VisLight_data(),results.get_InfLight_data());
-			out.println("Taos visible light:     "+taosCalcData[0]);
-			out.println("Taos infrared light:    "+taosCalcData[1]);
+//			printValues(results);
 		}
 	}
 	
+	private void printValues(MoteMsg results) {
+		int[] taosCalcData = null;
+		double[] sensirionCalcData=null;
+		results.get_nodeId();
+		System.out.println("The measured results are " + i++);
+		
+		System.out.println("Mote id:   "+results.get_nodeId());
+		System.out.println("Accelerometer X axis:   "+results.get_AccelX_data());
+		System.out.println("Accelerometer Y axis:   "+results.get_AccelY_data());
+		System.out.println("Intersema temperature:  "+(double)results.getElement_Intersema_data(0)/10);
+		System.out.println("Intersema pressure:     "+(double)results.getElement_Intersema_data(1)/10);
+		sensirionCalcData= mts400.calculateSensirion(results.get_tempVal(),results.get_humVal());
+		System.out.printf("Sensirion temperature:  %.2f\n",sensirionCalcData[0]);
+		System.out.printf("Sensirion humidity:     %.2f\n",sensirionCalcData[1]);
+		taosCalcData=mts400.calculateTaos(results.get_VisLight_data(),results.get_InfLight_data());
+		System.out.println("Taos visible light:     "+taosCalcData[0]);
+		System.out.println("Taos infrared light:    "+taosCalcData[1]);
+	}
+
 	public static void main (String[] args) {
 		if ( args.length == 2 && args[0].equals("-comm") ) {
 			out.println(args[1]);
@@ -50,7 +63,6 @@ class DataReceiver implements MessageListener{
 			System.err.println("usage: java DataReceiver [-comm <source>]");
 			System.exit(1);
 		}
-		
 	}
 
 }
--- DataReceiver/src/pl/edu/pw/dbWriters/MoteMsgDbWriter.java
+++ DataReceiver/src/pl/edu/pw/dbWriters/MoteMsgDbWriter.java
@@ -1,7 +1,5 @@
 package pl.edu.pw.dbWriters;
 
-import static java.lang.System.out;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,75 +10,105 @@ import pl.edu.pw.MoteMsg;
 import pl.edu.pw.Sensors.Mts400;
 
 public class MoteMsgDbWriter {
+	private static String connectionSting = "jdbc:mysql://localhost:3306/temps";
+	private static String databaseUserName = "root";
+	private static String databaseUserPassword = "nzozpoz.";
+	
 	public int writeReadingToDb(MoteMsg message){
-		Mts400 mts400 = new Mts400();
-		Integer moteId = message.get_nodeId();
 		int[] taosCalcData = null;
 		double[] sensirionCalcData=null;
-		if(moteExistInDB(moteId) < 1)
-			insertMoteIntoDb(moteId);
-		writeReadingToDb(message.get_nodeId(), "ADXL202JE-X", (double) message.get_AccelX_data());
-		writeReadingToDb(message.get_nodeId(), "ADXL202JE-Y", (double) message.get_AccelY_data());
-		writeReadingToDb(message.get_nodeId(), "Intersema-temp",(double)message.getElement_Intersema_data(0)/10);
-		writeReadingToDb(message.get_nodeId(), "Intersema-press",(double)message.getElement_Intersema_data(1)/10);
-		sensirionCalcData=mts400.calculateSensirion(message.get_tempVal(),message.get_humVal());
-		writeReadingToDb(message.get_nodeId(), "SHT11-temp", sensirionCalcData[0]);
-		writeReadingToDb(message.get_nodeId(), "SHT11-hum", sensirionCalcData[1]);
-		taosCalcData=mts400.calculateTaos(message.get_VisLight_data(),message.get_InfLight_data());
-		writeReadingToDb(message.get_nodeId(), "Taos ch1", (double) taosCalcData[0]);
-		writeReadingToDb(message.get_nodeId(), "Taos ch2", (double) taosCalcData[1]);
+		Connection c = null;
+		Mts400 mts400 = new Mts400();
+		
+		Integer moteId = message.get_nodeId();
+		
+
+		
+		try {
+			//check if mote exists in database, if not crate inserts mote
+			if(moteExistInDB(moteId) < 1)
+				insertMoteIntoDb(moteId);
+			c = DriverManager.getConnection(connectionSting, databaseUserName, databaseUserPassword);
+			//get current timestamp	
+			long time = System.currentTimeMillis();
+			
+			writeReadingToDb(message.get_nodeId(), "ADXL202JE-X", (double) message.get_AccelX_data(),c, time);
+			writeReadingToDb(message.get_nodeId(), "ADXL202JE-Y", (double) message.get_AccelY_data(),c, time);
+			writeReadingToDb(message.get_nodeId(), "Intersema-temp",(double)message.getElement_Intersema_data(0)/10,c, time);
+			writeReadingToDb(message.get_nodeId(), "Intersema-press",(double)message.getElement_Intersema_data(1)/10,c, time);
+			sensirionCalcData=mts400.calculateSensirion(message.get_tempVal(),message.get_humVal());
+			writeReadingToDb(message.get_nodeId(), "SHT11-temp", sensirionCalcData[0],c, time);
+			writeReadingToDb(message.get_nodeId(), "SHT11-hum", sensirionCalcData[1],c, time);
+			taosCalcData=mts400.calculateTaos(message.get_VisLight_data(),message.get_InfLight_data());
+			writeReadingToDb(message.get_nodeId(), "Taos ch1", (double) taosCalcData[0],c, time);
+			writeReadingToDb(message.get_nodeId(), "Taos ch2", (double) taosCalcData[1],c, time);
+			
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				c.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return 0;
 	}
 	private void insertMoteIntoDb(Integer moteId) {
 		Connection c = null;
 		PreparedStatement stmt = null;
 	    try {
-	    	Class.forName("com.mysql.jdbc.Driver");
-	      c = DriverManager.getConnection("jdbc:mysql://localhost:3306/temps","root", "nzozpoz.");
+			c = DriverManager.getConnection(connectionSting, databaseUserName, databaseUserPassword);
+			String sql = "insert into mote (id, sensorboard_id, name) values (?, ?, ?);" ;
+			stmt = c.prepareStatement(sql);
+			stmt.setInt(1, moteId);
+			stmt.setString(2, "mts400");
+			stmt.setString(3, "Mote " + moteId);
+			stmt.execute();
+			
+			stmt.close();
 	      
-	      String sql = "insert into mote (id, sensorboard_id, name) "
-	      				+ "values (?, ?, ?); " ;
-	      stmt = c.prepareStatement(sql);
-	      stmt.setInt(1, moteId);
-	      stmt.setString(2, "mts400");
-	      stmt.setString(3, "Mote " + moteId);
-	      stmt.execute();
-	
-	      stmt.close();
-	      c.close();
 	    } catch ( Exception e ) {
 	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }finally{
+	    	if (c != null)
+				try {
+					c.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 	    }
 	}
 	private int moteExistInDB(Integer moteId){
 		Connection c = null;
 		PreparedStatement stmt = null;
 		int numberOfMotes = -1;
-	    try {
-	    	Class.forName("com.mysql.jdbc.Driver");
-	      c = DriverManager.getConnection("jdbc:mysql://localhost:3306/temps","root", "nzozpoz.");
-	      c.setAutoCommit(false);
-	      
-	      String sql = "select count(*) as number from mote where id = ?;";
-	      
-	      stmt = c.prepareStatement(sql);
-	      stmt.setInt(1, moteId);
-	      ResultSet rs = stmt.executeQuery();
-	
-	      while ( rs.next() ) {
-	           numberOfMotes = rs.getInt("number");
-	       }
-	      
-	    } catch (Exception e ) {
-	    	System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	    	return -1;
-	    }
-	    finally{
-	    	try {
-	    		if(stmt != null)
-	    			stmt.close();
-	    		if(c != null)
-	    			c.close();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/temps","root", "nzozpoz.");
+			c.setAutoCommit(false);
+			 
+			String sql = "select count(*) as number from mote where id = ?;";
+			  
+			stmt = c.prepareStatement(sql);
+			stmt.setInt(1, moteId);
+			ResultSet rs = stmt.executeQuery();
+			
+			while ( rs.next() ) {
+				numberOfMotes = rs.getInt("number");
+			}
+			  
+		} catch (Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			return -1;
+		}
+		finally{
+			try {
+				if(stmt != null)
+					stmt.close();
+				if(c != null)
+					c.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
@@ -88,28 +116,22 @@ public class MoteMsgDbWriter {
 	    
 	    return numberOfMotes;
 	}
-	public int writeReadingToDb(Integer moteId, String sensorId, Double value){
-		Connection c = null;
+	public int writeReadingToDb(Integer moteId, String sensorId, Double value, Connection c , Long timeUTC){
 		PreparedStatement stmt = null;
 	    try {
-	    	Class.forName("com.mysql.jdbc.Driver");
-	      c = DriverManager.getConnection("jdbc:mysql://localhost:3306/temps","root", "nzozpoz.");
 	      
-	      String sql = "insert into reading (mote_id, sensor_id, value) "
-	      				+ "values (?, ?, ?); " ;
-	      stmt = c.prepareStatement(sql);
-	      stmt.setInt(1, moteId);
-	      stmt.setString(2, sensorId);
-	      stmt.setDouble(3, value);
-	      stmt.execute();
-	
-	      stmt.close();
-	      c.close();
+	    	String sql = "insert into reading (mote_id, sensor_id, value, timestamp) "
+	      				+ "values (?, ?, ?,?); " ;
+	    	stmt = c.prepareStatement(sql);
+	    	stmt.setInt(1, moteId);
+	    	stmt.setString(2, sensorId);
+	    	stmt.setDouble(3, value);
+	    	stmt.setLong(4, timeUTC);
+	    	stmt.execute();
 	    } catch ( Exception e ) {
 	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	      return -1;
 	    }
 	    return 0;
 	  }
-
 }
